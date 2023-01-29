63b49a0a32fa55af0f2bc79b82bf5281aaeba60f
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-29 12:35:40 +0300

    add

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,23 +1,36 @@
+import java.io.File
+
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
+private val firstNames = setOf("Alex", "Mordoc", "Sophie", "Tariq")
+private val lastNames = setOf("Ironfoot", "Fernsworth", "Baggins", "Downstrider")
+
+private val menuData = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
+
+private val menuItems = List(menuData.size) { index ->
+    val (_, name, _) = menuData[index].split(",")
+    name
+}
 
 fun visitTavern(){
-     narrate("$heroName enters $TAVERN_NAME")
-    val patrons= mutableListOf("Eli", "Mordoc", "Sophie")
-    val eliMaster = if(patrons.contains("Eli")){
-        "$TAVERN_MASTER says: Eli's in the back playing cards"
-    }else{
-        "$TAVERN_MASTER says: Eli isn't here"
+    narrate("$heroName enters $TAVERN_NAME")
+    narrate("There are several items for sale:")
+    narrate(menuItems.joinToString())
+    val patrons: MutableSet<String> = mutableSetOf()
+    while (patrons.size < 10){
+        patrons += "${firstNames.random()} ${lastNames.random()}"
     }
-    println(eliMaster)
-    val othersMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
-        "$TAVERN_MASTER says: Sophie and Mordoc are seated by the stew kettle"
-    } else {
-        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    narrate("$heroName sees several patrons in the tavern: ")
+    narrate(patrons.joinToString())
+    repeat(3){
+        placeOrder(patrons.random(),menuItems.random())
     }
-    println(othersMessage)
-    patrons.forEachIndexed { index, patron ->
-        println("Good evening, $patron - yoy are #${index + 1}")
-    }
+}
+
+private fun placeOrder(patronName: String, menuItemName: String){
+    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
+    narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
 }
